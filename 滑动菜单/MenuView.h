9063fcf6b5991d5f9ece6d8499f533367c05c135
baoxu <baoxu@bestpay.com.cn>
2017-11-13 21:40:23 +0800

    效果一完成

---
+++ 滑动菜单/MenuView.h
@@ -0,0 +1,13 @@
+//
+//  MenuView.h
+//  滑动菜单
+//
+//  Created by yfzx-sh-baoxu on 2017/11/12.
+//  Copyright © 2017年 鲍旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuView : UIView
+
+@end
---
+++ 滑动菜单/MenuView.m
@@ -0,0 +1,21 @@
+//
+//  MenuView.m
+//  滑动菜单
+//
+//  Created by yfzx-sh-baoxu on 2017/11/12.
+//  Copyright © 2017年 鲍旭. All rights reserved.
+//
+
+#import "MenuView.h"
+
+@implementation MenuView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
